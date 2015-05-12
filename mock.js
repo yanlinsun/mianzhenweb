f09a275ca96255ee02b3c0375623201aa61ea1ee
孙延林 <sunyanlin@sunyanlindeMacBook-Pro.local>
2015-05-13 10:30:08 +1200

    draft version support face landmarks

--- mock.js
+++ mock.js
@@ -1,48 +1,47 @@
-var DetectResult = {
-    Faces : [
-              {
-              "faceId": "c5c24a82-6845-4031-9d5d-978df9175426",
-              "faceRectangle": {
-              "top": 54,
-              "left": 394,
-              "width": 78,
-              "height": 78
-              },
-              "faceLandmarks": {
-              "pupilLeft": { "x": 412.7, "y": 78.4 },
-              "pupilRight": { "x": 446.8, "y": 74.2 },
-              "noseTip": { "x": 437.7, "y": 92.4 },
-              "mouthLeft": { "x": 417.8, "y": 114.4 },
-              "mouthRight": { "x": 451.3, "y": 109.3 },
-              "eyebrowLeftOuter": { "x": 397.9, "y": 78.5 },
-              "eyebrowLeftInner": { "x": 425.4, "y": 70.5 },
-              "eyeLeftOuter": { "x": 406.7, "y": 80.6 },
-              "eyeLeftTop": { "x": 412.2, "y": 76.2 },
-              "eyeLeftBottom": { "x": 413.0, "y": 80.1 },
-              "eyeLeftInner": { "x": 418.9, "y": 78.0 },
-              "eyebrowRightInner": { "x": 4.8, "y": 69.7 },
-              "eyebrowRightOuter": { "x": 5.5, "y": 68.5 },
-              "eyeRightInner": { "x": 441.5, "y": 75.0 },
-              "eyeRightTop": { "x": 446.4, "y": 71.7 },
-              "eyeRightBottom": { "x": 447.0, "y": 75.3 },
-              "eyeRightOuter": { "x": 451.7, "y": 73.4 },
-              "noseRootLeft": { "x": 428.0, "y": 77.1 },
-              "noseRootRight": { "x": 435.8, "y": 75.6 },
-              "noseLeftAlarTop": { "x": 428.3, "y": 89.7 },
-              "noseRightAlarTop": { "x": 442.2, "y": 87.0 },
-              "noseLeftAlarOutTip": { "x": 424.3, "y": 96.4 },
-              "noseRightAlarOutTip": { "x": 446.6, "y": 92.5 },
-              "upperLipTop": { "x": 437.6, "y": 105.9 },
-              "upperLipBottom": { "x": 437.6, "y": 108.2 },
-              "underLipTop": { "x": 436.8, "y": 111.4 },
-              "underLipBottom": { "x": 437.3, "y": 114.5 }
-              },
-
-              "attributes": {
-              "headPose": { "pitch": 0.0, "roll": -10.3, "yaw": 18.1 },
-              "gender": "male",
-              "age": 71.0
-              }
-              }
-              ]
-};
+var DetectResult = [
+    { 
+        "faceId" : "6a0a4b14-5a80-4eb1-9d93-06032459de31",
+        "faceRectangle" : { 
+            "top" : 174,
+            "left" : 142,
+            "width" : 461,
+            "height" : 461
+        },
+        "faceLandmarks" : { 
+            "pupilLeft" : { "x" : 230.7,"y" : 332.6 },
+            "pupilRight" : { "x" : 432.6,"y" : 268.5 },
+            "noseTip" : { "x" : 367.9,"y" : 440.6 },
+            "mouthLeft" : { "x" : 341.1,"y" : 527.4 },
+            "mouthRight" : { "x" : 496.0,"y" : 473.4 },
+            "eyebrowLeftOuter" : { "x" : 124.8,"y" : 298.9 },
+            "eyebrowLeftInner" : { "x" : 242.8,"y" : 273.0 },
+            "eyeLeftOuter" : { "x" : 194.8,"y" : 341.6 },
+            "eyeLeftTop" : { "x" : 225.6,"y" : 320.3 },
+            "eyeLeftBottom" : { "x" : 232.9,"y" : 351.1 },
+            "eyeLeftInner" : { "x" : 261.6,"y" : 333.2 },
+            "eyebrowRightInner" : { "x" : 347.3,"y" : 245.2 },
+            "eyebrowRightOuter" : { "x" : 497.4,"y" : 187.5 },
+            "eyeRightInner" : { "x" : 404.2,"y" : 287.0 },
+            "eyeRightTop" : { "x" : 428.6,"y" : 259.2 },
+            "eyeRightBottom" : { "x" : 443.8,"y" : 281.9 },
+            "eyeRightOuter" : { "x" : 471.3,"y" : 256.9 },
+            "noseRootLeft" : { "x" : 298.6,"y" : 331.7 },
+            "noseRootRight" : { "x" : 357.3,"y" : 314.3 },
+            "noseLeftAlarTop" : { "x" : 316.6,"y" : 413.6 },
+            "noseRightAlarTop" : { "x" : 397.3,"y" : 384.4 },
+            "noseLeftAlarOutTip" : { "x" : 322.4,"y" : 458.2 },
+            "noseRightAlarOutTip" : { "x" : 432.4,"y" : 418.6 },
+            "upperLipTop" : { "x" : 395.0,"y" : 496.7 },
+            "upperLipBottom" : { "x" : 402.3,"y" : 514.9 },
+            "underLipTop" : { "x" : 411.6,"y" : 525.7 },
+            "underLipBottom" : { "x" : 423.4,"y" : 547.3 }
+        },
+        "attributes" : { "gender" : "female","age" : 19,
+        "headPose": {
+            "roll": "-3.0",
+            "yaw": "8.8",
+            "pitch": "0.0"
+            }
+        }
+    }
+];
