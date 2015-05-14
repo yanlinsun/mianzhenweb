af4d6be7852735c03fb7563c9b3fd48565e88f78
Yanlin Sun <yanlin.sun@gmail.com>
2015-05-14 23:54:19 +1200

    almost done

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var PP = [
     "发", "额", "眉", "睑", "睛", "瞳", "颧", "鼻", "耳", "颊", "颌" 
 ];
 
-var test = false;
+var mock = true;
+var debug = true;
 var iOS = false;
 var imageReady = false;
 var current_faces = [];
@@ -26,15 +27,12 @@ function error(msg) {
         $("#msg").fadeOut(3000);
 }
 
-function calculate(name) {
+function toggle(name) {
     if (!imageReady) {
         error("Image not ready");
     }
     switch (name) {
     }
-}
-
-function clearPP() {
 }
 
 function initButton(name) {
@@ -43,7 +41,7 @@ function initButton(name) {
         .click(function() {
             $(this).addClass("active")
                 .siblings().removeClass("active");
-            calculate($(this).text());
+            toggle($(this).text());
         });
 }
 
@@ -51,6 +49,7 @@ function initButtons() {
     for (var i in PP) {
         $("#buttonGroup1").append(initButton(PP[i]));
     }
+    $("#buttonGroup1").hide();
 }
 
 function imageSelected(e) {
@@ -80,7 +79,7 @@ function imageSelected(e) {
 $(function() {
     $("#msg").hide();
     $("#imgObj").hide();
-    //initButtons();
+    initButtons();
     $("#imgFile").change(imageSelected).click(function() { $(this).attr("value", null); });
 });
 
@@ -90,7 +89,7 @@ function detectFace(img, md, orientation) {
         error(errSize);
     }
     img = $("#imgFile").get(0).files[0];
-    if (!test) {
+    if (!mock) {
         faceService.detectFaces({
             data: img,
             success: function(faces) {
@@ -170,12 +169,8 @@ function adjustSpotOrientation(spot) {
 }
 
 function scaleFace(face, factor) {
-    var f = {
-        faceId : face.faceId
-    };
-
     if (face.faceRectangle) {
-        f.faceRectangle = adjustRectOrientation({
+        face.scaledFaceRectangle = adjustRectOrientation({
             top: factorY(face.faceRectangle.top),
             left: factorX(face.faceRectangle.left),
             width: factorX(face.faceRectangle.width, true),
@@ -183,90 +178,254 @@ function scaleFace(face, factor) {
         });
     }
     if (face.faceLandmarks) {
-        f.faceLandmarks = {};
+        face.scaledFaceLandmarks = {};
         for (var mark in face.faceLandmarks) {
-            f.faceLandmarks[mark] = adjustSpotOrientation({
+            face.scaledFaceLandmarks[mark] = adjustSpotOrientation({
                 x : factorX(face.faceLandmarks[mark].x),
                 y : factorY(face.faceLandmarks[mark].y)
             });
         }
     }
-    if (face.attributes) {
-        f.attributes = {
-            age : face.attributes.age,
-            gender : face.attributes.gender
-        };
-        if (face.attributes.headPose) {
-            f.attributes.headPose = {
-                roll : face.attributes.headPose.roll,
-                yaw : face.attributes.headPose.yaw,
-                pitch : face.attributes.headPose.pitch
-            };
-        }
-    }
-    calcFaceParts(f);
-    return f;
+    calcFaceParts(face);
+    return face;
 }
 
 function calcFaceParts(f) {
-    f.parts = {};
-    var l = f.faceLandmarks["noseRootLeft"];
-    var r = f.faceLandmarks["noseRootRight"];
-    var angleB = calcAngle(l, r);
-    var base = f.faceLandmarks["noseTip"];
-    f.rotatedLandmarks = {
-        "noseTip" : base
+    rotateFaceLandmarks2Vertical(f);
+    var p = f.rotatedScaledFaceLandmarks;
+    var o = {};
+    var inchLeft = p.noseLeftAlarTop.y - p.noseRootLeft.y;
+    var inchRight = p.noseRightAlarTop.y - p.noseRootRight.y;
+    o.eyebrowTopLeft = {
+        x : p.eyebrowLeftOuter.x + (p.eyebrowLeftInner.x - p.eyebrowLeftOuter.x) / 3,
+        y : p.eyebrowLeftOuter.y - (p.pupilLeft.y - p.eyeLeftTop.y)
     };
-    for (var i in f.faceLandmarks) {
+    o.eyebrowTopRight = {
+        x : p.eyebrowRightOuter.x - (p.eyebrowRightOuter.x - p.eyebrowRightInner.x) / 3,
+        y : p.eyebrowRightOuter.y - (p.pupilRight.y - p.eyeRightTop.y)
+    };
+    o.foreheadLowerLeft = {
+        x : p.eyeLeftTop.x,
+        y : o.eyebrowTopLeft.y
+    };
+    o.foreheadLowerRight = {
+        x : p.eyeRightTop.x,
+        y : o.eyebrowTopRight.y
+    };
+    o.foreheadUpperLeft = {
+        x : o.foreheadLowerLeft.x,
+        y : o.foreheadLowerLeft.y - inchLeft 
+    };
+    o.foreheadUpperRight = {
+        x : o.foreheadLowerRight.x,
+        y : o.foreheadLowerRight.y - inchRight
+    };
+    o.eyelidLowerTipLeft = {
+        x : p.eyeLeftBottom.x,
+        y : p.eyeLeftBottom.y + inchLeft / 3
+    };
+    o.eyelidLowerTipRight = {
+        x : p.eyeRightBottom.x,
+        y : p.eyeRightBottom.y + inchRight / 3
+    };
+    o.cheekUpperOuterLeft = {
+        x : p.eyebrowLeftOuter.x,
+        y : o.eyelidLowerTipLeft.y
+    };
+    o.cheekUpperOuterRight = {
+        x : p.eyebrowRightOuter.x,
+        y : o.eyelidLowerTipRight.y
+    };
+    o.cheekUpperInnerLeft = {
+        x : p.eyeLeftInner.x,
+        y : o.eyelidLowerTipLeft.y
+    };
+    o.cheekUpperInnerRight = {
+        x : p.eyeRightInner.x,
+        y : o.eyelidLowerTipRight.y
+    };
+    o.cheekLowerOuterLeft = {
+        x : p.eyebrowLeftOuter.x / 2 + p.eyeLeftOuter.x / 2,
+        y : p.noseLeftAlarOutTip.y
+    };
+    o.cheekLowerOuterRight = {
+        x : p.eyebrowRightOuter.x / 2 + p.eyeRightOuter.x / 2,
+        y : p.noseRightAlarOutTip.y
+    };
+    o.cheekLowerInnerLeft = {
+        x : p.eyeLeftInner.x - (p.eyeLeftInner.x - p.eyeLeftTop.x) / 4,
+        y : o.cheekLowerOuterLeft.y
+    };
+    o.cheekLowerInnerRight = {
+        x : p.eyeRightInner.x + (p.eyeRightInner.x - p.eyeRightTop.x) / 4,
+        y : o.cheekLowerOuterRight.y
+    };
+    o.cheekLowerTipLeft = {
+        x : p.eyeLeftOuter.x,
+        y : p.noseTip.y / 2 + p.upperLipTop.y / 2
+    };
+    o.cheekLowerTipRight = {
+        x : p.eyeRightOuter.x,
+        y : o.cheekLowerTipLeft.y 
+    };
+    o.cheekBottomLeft = {
+        x : o.cheekUpperOuterLeft.x,
+        y : p.mouthLeft.y
+    };
+    o.cheekBottomRight = {
+        x : o.cheekUpperOuterRight.x,
+        y : p.mouthRight.y
+    };
+    o.jawLowerLeft = {
+        x : p.noseLeftAlarOutTip.x,
+        y : p.mouthLeft.y + inchLeft
+    };
+    o.jawLowerRight = {
+        x : p.noseRightAlarOutTip.x,
+        y : p.mouthRight.y + inchRight
+    };
+
+    f.rotatedCalculatedFaceLandmarks = o;
+    rotateFaceLandmarks2BaseAngle(f);
+}
+
+function rotateFaceLandmarks2Vertical(f) {
+    var l = f.scaledFaceLandmarks["noseRootLeft"];
+    var r = f.scaledFaceLandmarks["noseRootRight"];
+    factor.base = {
+        x : f.scaledFaceLandmarks["noseTip"].x,
+        y : f.scaledFaceLandmarks["noseTip"].y
+    };
+    factor.angleB = calcAngle(l, r);
+    f.rotatedScaledFaceLandmarks = {
+        "noseTip" : factor.base
+    };
+    for (var i in f.scaledFaceLandmarks) {
         if (i !== "noseTip") {
-            f.rotatedLandmarks[i] = rotate2Vertical(base, angleB, f.faceLandmarks[i]);
+            f.rotatedScaledFaceLandmarks[i] = rotate2Vertical(factor.base, factor.angleB, f.scaledFaceLandmarks[i]);
         }
     }
+}
+
+function rotateFaceLandmarks2BaseAngle(f) {
+    if (f.rotatedCalculatedFaceLandmarks) {
+        f.calculatedFaceLandmarks = {};
+        for (var i in f.rotatedCalculatedFaceLandmarks) {
+            f.calculatedFaceLandmarks[i] = rotate2BaseAngle(factor.base, factor.angleB, f.rotatedCalculatedFaceLandmarks[i]);
+        }
+    }
+}
+
+function rotate2BaseAngle(base, angleB, pos) {
+    var d = Math.sqrt(Math.pow(pos.x - base.x, 2) + Math.pow(pos.y - base.y, 2));
+    var angleP = calcAngle(base, pos);
+    var r = {
+        x : base.x,
+        y : base.y,
+        d : d,
+        angleP : angleP
+    };
+    var sinP_B, cosP_B;
+    if (angleP.dX === angleB.dX && angleP.dY === angleB.dY || 
+       (angleP.dX !== angleB.dX && angleP.dY !== angleB.dY)) {
+        sinP_B = angleP.cos * angleB.sin + angleP.sin * angleB.cos;
+        cosP_B = angleP.cos * angleB.cos - angleP.sin * angleB.sin;
+    } else {
+        sinP_B = angleP.sin * angleB.cos - angleP.cos * angleB.sin;
+        cosP_B = angleP.cos * angleB.cos + angleP.sin * angleB.sin;
+    }
+    var offsetX = d * cosP_B;
+    var offsetY = d * sinP_B;
+    r.offsetX = offsetX;
+    r.offsetY = offsetY;
+    r.sin = sinP_B;
+    r.cos = sinP_B;
+    r.branch = 1;
+    r.x += angleP.dX * offsetX;
+    r.y += angleP.dY * offsetY;
+    return r;
 }
 
 function rotate2Vertical(base, angleB, pos) {
     var d = Math.sqrt(Math.pow(pos.x - base.x, 2) + Math.pow(pos.y - base.y, 2));
     var angleP = calcAngle(base, pos);
-    var sinP_B = angleP.sin * angleB.cos - angleP.cos * angleB.sin;
-    var cosP_B = angleP.cos * angleB.cos + angleP.sin * angleB.sin;
-    if (angleP.sin < angleB.sin) {
-        sinP_B = -sinP_B;
+    var r = {
+        x : base.x,
+        y : base.y,
+        d : d,
+        angleP : angleP
+    };
+    var sinP_B, cosP_B;
+    if (angleP.dX === angleB.dX && angleP.dY === angleB.dY || 
+       (angleP.dX !== angleB.dX && angleP.dY !== angleB.dY)) {
+        sinP_B = angleP.sin * angleB.cos - angleP.cos * angleB.sin;
+        cosP_B = angleP.cos * angleB.cos + angleP.sin * angleB.sin;
+    } else {
+        sinP_B = angleP.cos * angleB.sin + angleP.sin * angleB.cos;
+        cosP_B = angleP.cos * angleB.cos - angleP.sin * angleB.sin;
     }
     var offsetX = d * cosP_B;
     var offsetY = d * sinP_B;
-    var r = {
-        x : base.x,
-        y : base.y
-    };
-    if (angleP.sin < angleB.sin) {
-        if (angleP.dX !== angleP.dY && angleP.dX === angleB.dX && angleB.dX !== angleB.dY) {
-            r.x += angleP.dX * offsetX;
-            r.y -= angleP.dY * offsetY;
-        } else {
-            r.x += angleP.dX * offsetX;
-            r.y += angleP.dY * offsetY;
-        }
-    } else if (angleP.sin > (1 - angleB.sin)) {
-        if (angleP.dX === angleP.dY && angleP.dX !== angleB.dX && angleB.dX !== angleB.dY) {
-            r.x -= angleP.dX * offsetX;
-            r.y += angleP.dY * offsetY;
-        } else {
-            r.x += angleP.dX * offsetX;
-            r.y += angleP.dY * offsetY;
-        }
-    }
+    r.offsetX = offsetX;
+    r.offsetY = offsetY;
+    r.sin = sinP_B;
+    r.cos = sinP_B;
+    r.branch = 1;
+    r.x += angleP.dX * offsetX;
+    r.y += angleP.dY * offsetY;
     return r;
 }
 
 function calcAngle(base, pos) {
     var tan = Math.abs((pos.y - base.y) / (pos.x - base.x));
     return {
-        dX : pos.x >= base.x,
-        dY : pos.y >= base.y,
+        dX : pos.x >= base.x ? 1 : -1,
+        dY : pos.y >= base.y ? 1 : -1,
         tan : tan,
         sin : tan / Math.sqrt(1 + Math.pow(tan, 2)),
         cos : 1 / Math.sqrt(1 + Math.pow(tan, 2))
     };
+}
+
+function drawFacePolygons(f) {
+    if (f.calculatedFaceLandmarks) {
+        var imgObj = $("#imgDiv");
+        $("#svg").css("position", "absolute")
+            .css("left", imgObj.offset().left)
+            .css("top", imgObj.offset().top)
+            .css("width", imgObj.width())
+            .css("height", imgObj.height());
+
+        // jquery.appendTo or html container.appendChild not working for polygon, not knowing why
+        // maybe the created polygon has no parent element
+        var container = $("#svg");
+        var o = f.calculatedFaceLandmarks;
+        var polygons = [
+            newPolygon([
+                o["foreheadUpperLeft"],
+                o["foreheadLowerLeft"],
+                o["foreheadLowerRight"],
+                o["foreheadUpperRight"]
+            ]),
+            newPolygon([
+                o.cheekUpperOuterLeft,
+                o.cheekLowerOuterLeft,
+                o.cheekLowerTipLeft,
+                o.cheekLowerInnerLeft,
+                o.cheekUpperInnerLeft
+            ]),
+        ];
+        container[0].innerHTML = polygons.join();
+    }
+}
+
+function newPolygon(p) {
+    var html = "<polygon points=\"";
+    for (var i in p) {
+        html += p[i].x + "," + p[i].y + " ";
+    }
+    html += "\" style=\"fill:brown;opacity:0.5\" />";
+    return html;
 }
 
 function drawFaceRects() {
@@ -276,10 +435,19 @@ function drawFaceRects() {
         $.each(current_faces, function(t, face) {
             var f = scaleFace(face);
             addFaceRectangle(f, container);
-            addFaceLandmarks(f.faceLandmarks, container, (f.attributes.gender === "female" ? "femalefacespot" : "malefacespot"));
-//            if (f.rotatedLandmarks) {
-//                addFaceLandmarks(f.rotatedLandmarks, container, "rotatefacespot");
-//            }
+            if (debug) {
+                addFaceLandmarks(f.scaledFaceLandmarks, container, (f.attributes.gender === "female" ? "femalefacespot" : "malefacespot"));
+                if (f.rotatedScaledFaceLandmarks) {
+                    addFaceLandmarks(f.rotatedScaledFaceLandmarks, container, "rotatefacespot");
+                }
+                if (f.rotatedCalculatedFaceLandmarks) {
+                    addFaceLandmarks(f.rotatedCalculatedFaceLandmarks, container, "rotatedcalculatedfacespot");
+                }
+                if (f.calculatedFaceLandmarks) {
+                    addFaceLandmarks(f.calculatedFaceLandmarks, container, "calculatedfacespot");
+                }
+            }
+            drawFacePolygons(f);
         })
     }
 }
@@ -315,7 +483,7 @@ function deleteFaceRects() {
 }
 
 function addFaceRectangle(face, container) {
-    var pos = face.faceRectangle;
+    var pos = face.scaledFaceRectangle;
     var txt = "";
     if (face.attributes.gender && face.attributes.gender === "female") {
         txt = "女";
